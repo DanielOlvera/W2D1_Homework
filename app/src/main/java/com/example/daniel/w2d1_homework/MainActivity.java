e5dc16900d8ff21496f0d777e4b7ec42222b0773
DanielOlvera <josue_olver@yahoo.com.mx>
2016-10-30 21:25:58 -0400

    Finished the PreferencesFragment part of the application, which will be integrated with the overflow menu

--- app/src/main/java/com/example/daniel/w2d1_homework/MainActivity.java
+++ app/src/main/java/com/example/daniel/w2d1_homework/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.daniel.w2d1_homework;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, MyPreferencesActivity.class);
+        startActivity(intent);
     }
 }
+
+
+/*
+References:
+    http://alvinalexander.com/android/android-tutorial-preferencescreen-preferenceactivity-preferencefragment
+    http://www.cs.dartmouth.edu/~campbell/cs65/lecture12/lecture12.html
+ */
